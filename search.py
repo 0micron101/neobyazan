fb4b3f2bd916c133806463ad1044224f7704bbf4
0micron101 <116595447+0micron101@users.noreply.github.com>
2025-04-27 12:15:43 +0300

    Update search.py

--- search.py
+++ search.py
@@ -30,7 +30,7 @@ extagsrch=st.multiselect("Исключить метки из поиска",allta
 
 for i in os.listdir("json"):
    with open(fr"json/{i}","r",encoding="utf-8") as fl:
-       current=json.load(fl)
+       current=json.loads(fl)
        if namesrch=="alltags":
            st.write(current)
          
